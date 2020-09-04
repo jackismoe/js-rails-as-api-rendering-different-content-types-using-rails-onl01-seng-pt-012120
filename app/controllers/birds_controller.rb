c3407dd6e9e5c097401cfa2668cdbb5feab4f86f
jackismoe <jacksonmo@me.com>
2020-09-04 10:50:20 -0400

    Resubmit

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,17 @@
 class BirdsController < ApplicationController
+  # def index
+  #   @birds = Bird.all
+  #   render plain: "Hello #{@birds[3].name}" 
+  # end
+
   def index
-    @birds = Bird.all
-    render json: {birds: @birds, messages: ['Hello Birds', 'Goodbye Birds']}
+    birds = Bird.all
+    # render json: 'Remember that JSON is just object notation converted to a string, so a string will work here too.'
+    # render json: {message: 'Hashes of data will get converted to JSON'}
+    # render json: ['As', 'well', 'as', 'arrays']
+
+    #render json: @birds
+
+    render json: {birds: birds, messages: ['Hello Birds', 'Goodbye Birds']}
   end
 end
